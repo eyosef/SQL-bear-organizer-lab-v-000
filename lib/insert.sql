be35a570dfdbdd58248f50b7c8d44dd50f12b8de
Eden Mesfun <emesfun@gmail.com>
2018-03-12 14:33:14 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO cats (id, name, age, gender, color, temperament, alive) VALUES ('Maru', 3, 'Scottish Fold');
+
+Mr. Chocolate
+Rowdy
+Tabitha
+Sergeant Brown
+Melissa
+Grinch
+Wendy
+unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
