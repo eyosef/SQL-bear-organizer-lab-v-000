59b70f43c5cf483411085963b2a844ffaf45ccf6
Eden Mesfun <emesfun@gmail.com>
2018-03-12 14:39:10 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,11 +6,5 @@ INSERT INTO cats (id, name, age, gender, color, temperament, alive) VALUES (5, '
 INSERT INTO cats (id, name, age, gender, color, temperament, alive) VALUES (6, 'Grinch', 15,'M', 'Grey', 'guarded', 'alive');
 INSERT INTO cats (id, name, age, gender, color, temperament, alive) VALUES (7, 'Wendy', 2,'F', 'White', 'friendly', 'alive');
 
-Mr. Chocolate
-Rowdy
-Tabitha
-Sergeant Brown
-Melissa
-Grinch
-Wendy
+
 unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
